b0e9711db05c7915d6a16e5ff6123f1fed045856
Guillaume Bouvier <bougui505@gmail.com>
2015-03-04 13:18:40 +0100

    replace the occurrence of #P and #T by the current page number and the total number of pages respectively 	modified:   svgtopdf.sh

--- svgtopdf.sh
+++ svgtopdf.sh
@@ -11,6 +11,8 @@
 # $1: toc file
 # $2: output pdf file name
 
+# Replace the occurence of #P by the page number and #T by the total number of pages
+
 # Dependencies: inkscape, pdftk or gs
 
 toc=$1
@@ -18,14 +20,18 @@ outpdf=$2
 
 i=0
 tmpdir=/dev/shm
+t=$(wc -l $toc | awk '{print $1}')
 for x in $(cat $toc); do
     i=$(($i+1))
     echo $i $x
-    inkscape --export-pdf=$tmpdir/$i.pdf $x
-    pdflist+=(" $tmpdir/$i.pdf") # append to an array (see: http://stackoverflow.com/a/18041780/1679629)
+    sed "s/#P/$i/" $x | sed "s/#T/$t/" > $tmpdir/$i.svg
+    inkscape --export-pdf=$tmpdir/$i.pdf $tmpdir/$i.svg
+    svglist+=(" $tmpdir/$i.svg") # append to an array (see: http://stackoverflow.com/a/18041780/1679629)
+    pdflist+=(" $tmpdir/$i.pdf")
 done
 
 echo ${pdflist[@]}
 #pdftk ${pdflist[@]} cat output $outpdf
 gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -sOutputFile=$outpdf ${pdflist[@]} # from: http://tex.stackexchange.com/a/8665/19419
-rm ${pdflist[@]}
+#rm ${pdflist[@]}
+#rm ${svglist[@]}
